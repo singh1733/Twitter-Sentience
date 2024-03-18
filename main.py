769db4852d45c6962c25c752162dad1629a84ba3
singh1733 <125903944+singh1733@users.noreply.github.com>
2024-03-17 22:57:17 -0400

    added retrieving the overall polarity

--- main.py
+++ main.py
@@ -20,5 +20,31 @@ tweet_amount=100
 
 tweets = client.search_recent_tweets(query=search_term, max_results=tweet_amount)
 
+polarity=0
+positive=0
+neutral=0
+negative=0
+
+#clean up (get rid of 'RT' and '@' handles if a tweet has them)
 for tweet in tweets.data:
-    print(tweet.id)
+    clean_text=tweet.id.text.replace('RT','')
+    if clean_text.startswith(' @'):
+        position=clean_text.index(':')
+        clean_text=clean_text[position+2:]
+    if clean_text.startswith('@'):
+      position=clean_text.index(' ')
+      clean_text=clean_text[position+2:]
+    #get overall polarity (how positive or negative)
+    analysis=TextBlob(clean_text)
+    tweet_polarity=analysis.polarity
+    if tweet_polarity>0.20:
+       positive+=1
+    elif tweet_polarity==0.00:
+       neutral+=1
+    elif tweet_polarity<0.20 :
+       negative+=1
+
+    polarity+=tweet_polarity
+
+print("The overall sentiment from -1 to 1 (1 being positive. -1 being negitive) is: {polarity}")
+   
